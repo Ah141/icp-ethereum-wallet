c04d8e1673f336f7d19ee8e134b2eef7e31c7e67
Ahmed Mohamed <ahmed.org858@gmail.com>
2025-09-11 11:39:11 +0300

    feat: implement init_state, read_state, and mutate_state helpers for accessing and modifying STATE

--- src/icp-ethereum-wallet-backend/src/state.rs
+++ src/icp-ethereum-wallet-backend/src/state.rs
@@ -5,7 +5,23 @@ use ic_cdk::api::management_canister::ecdsa::EcdsaKeyId;
 use std::cell::RefCell;
 use std::ops::{Deref, DerefMut};
 
-Tread_local! {
+thread_local! {
     /// Global state for the canister, stored in a thread-local RefCell for interior mutability.
     static STATE: RefCell<State> = RefCell::new(State::default());
+}
+
+
+pub fn init_state(init_args: InitArg) {
+    STATE.with(|s| *s.borrow_mut() = State::from(init_args));
+}
+
+pub fn read_state<R>(f: impl FnOnce(&State) -> R) -> R{
+    STATE.with(|s| f(s.borrow().deref()));
+}
+
+pub fn mutat_state<F, R>(f: F) -> R
+where
+    F: FnOnce(&mut State) -> R,
+{
+    STATE.with(|s| f(s.borrow_mut().deref_mut()))
 }
