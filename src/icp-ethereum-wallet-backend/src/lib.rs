6feac9c4468595621864edf305b0d4c344588394
Ahmed <ahmed.org858@gmail.com>
2025-09-14 23:24:54 +0300

    Add EVM RPC canister integration constant (EVM_RPC_CANISTER_ID)

--- src/icp-ethereum-wallet-backend/src/lib.rs
+++ src/icp-ethereum-wallet-backend/src/lib.rs
@@ -1,4 +1,22 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+mod ecdsa;
+mod ethereum_wallet;
+mod state;
+
+use crate::ethereum_wallet::EthereumWallet;
+use crate::state::{init_state, read_state};
+use alloy_consensus::{SignableTransaction, TxEip1559, TxEnvelope};
+use alloy_primitives::{hex, Signature, TxKind, U256};
+use candid::{CandidType, Deserialize, Nat, Principal};
+use evm_rpc_canister_types::{
+    BlockTag, EthMainnetService, EthSepoliaService, EvmRpcCanister, GetTransactionCountArgs,
+    GetTransactionCountResult, MultiGetTransactionCountResult, RequestResult, RpcService,
+};
+use ic_cdk::api::management_canister::ecdsa::{EcdsaCurve, EcdsaKeyId};
+use ic_cdk::{init, update};
+use ic_ethereum_types::Address;
+use num::{BigUint, Num};
+use std::str::FromStr;
+
+pub const EVM_RPC_CANISTER_ID: Principal =
+    Principal::from_slice(b"\x00\x00\x00\x00\x02\x30\x00\xCC\x01\x01"); // 7hfb6-caaaa-aaaar-qadga-cai
+pub const EVM_RPC: EvmRpcCanister = EvmRpcCanister(EVM_RPC_CANISTER_ID);
